ff3ea552651b6f0739bbaf55ac5c9516c1c41e2c
Andrei Cioara <andrei@cioara.me>
2018-08-04 18:53:58 +0100

    Name input

--- src/UI.jsx
+++ src/UI.jsx
@@ -1,37 +1,128 @@
 const { h, render, Component, Color } = require('ink');
 
 const Foo = require('import-jsx')('./Foo.jsx')
+const TextInput = require('ink-text-input');
 
-class Counter extends Component {
+class Name extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        }
+    }
+
+    render() {
+        return <TextInput
+            value={this.state.value}
+            onChange={value => this.setState({value})}
+            onSubmit={value => this.onSubmit(value)}
+        />
+    }
+
+    onSubmit(value) {
+        this.props.onSubmit(value)
+    }
+}
+
+class Offices extends Component {
+    render() {
+        return <div></div>
+    }
+
+}
+
+class Activities extends Component {
+    render() {
+        return <div></div>
+    }
+}
+
+class AllowedEmailDomains extends Component {
+    render() {
+        return <div></div>
+    }
+
+}
+
+class HasLogo extends Component {
+    render() {
+        return <div></div>
+    }
+
+}
+
+class Result extends Component {
+    render() {
+        return <div>
+
+        </div>
+    }
+}
+
+
+class UI extends Component {
     constructor() {
         super();
 
         this.state = {
-            i: 0
+            node: 0
         };
+        this.json = {}
     }
 
     render() {
-        return (
-            <Color green>
-                {this.state.i} tests passed
-                <br/>
-                <Foo></Foo>
-            </Color>
-        );
+        switch (this.state.node) {
+            case 0: {
+                return <Name
+                    onSubmit={value => {
+                        this.json.name = value;
+                        this.advanceNode();
+                    }}
+                />
+            }
+            case 1: {
+                return <Offices
+                    onSubmit={value => {}}
+                />
+            }
+            case 2: {
+                return <Activities
+                    onSubmit={value => {}}
+                />
+            }
+            case 3: {
+                return <AllowedEmailDomains
+                    onSubmit={value => {}}
+                />
+            }
+            case 4: {
+                return <HasLogo
+                    onSubmit={value => {}}
+                />
+            }
+            case 5: {
+                return <Result
+                    onSubmit={value => {}}
+                />
+            }
+            default: {
+                return <div>Done</div>
+            }
+        }
     }
 
     componentDidMount() {
-        this.timer = setInterval(() => {
-            this.setState({
-                i: this.state.i + 1
-            });
-        }, 100);
     }
 
     componentWillUnmount() {
         clearInterval(this.timer);
     }
+
+    advanceNode() {
+        let {node} = this.state;
+        node += 1;
+        this.setState({node});
+    }
 }
 
-module.exports = Counter;
+module.exports = UI;
